438d8ac519ccb4566a5ae008aad063de383931e1
Mariano Arias <hola@marianoarias.soy>
2021-11-30 22:21:43 -0300

    New commit

--- js/ejercicios_clase35.js
+++ js/ejercicios_clase35.js
@@ -1,4 +1,4 @@
-// Ejercios Clase 34
+// Ejercios Clase 35
 
 //  1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
 miFuncion1 = (string) => {
