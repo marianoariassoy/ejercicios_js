2281c1e9805ffab8f0d0537e614310c38f6d1568
Mariano Arias <hola@marianoarias.soy>
2021-12-01 11:47:02 -0300

    Optimización del codigo

--- js/ejercicios_clase35.js
+++ js/ejercicios_clase35.js
@@ -1,27 +1,29 @@
 // Ejercios Clase 35
 
 //  1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
-miFuncion1 = (string) => {
-   if (isNaN(string)) console.log(`La cadena de texto ingresda tiene ${string.length} caracteres`);
-   else console.log("No es una cadena de texto");
-};
-miFuncion1("Hola mundo");
+stringLength = (string) => (typeof string === "string" ? string.length : "El parametro ingresado no es una cadena válida");
+console.log(stringLength("Hola mundo"));
+console.log(stringLength(""));
+console.log(stringLength(25));
+console.log(stringLength());
 
 // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
-miFuncion2 = (string, num) => {
-   if (isNaN(string)) console.log(`Se muestran ${num} caracteres de la cadena ${string}, el restulado es: "${string.substring(0, num)}"`);
-   else console.log("No es una cadena de texto");
-};
-miFuncion2("Hola Mundo", 4);
+cutString = (string, char) => (stringLength(string) > char ? string.substring(0, char) : "No se puede cortar el valor ingresado.");
+console.log(cutString("Hola Mundo", 4));
+console.log(cutString("Hola Mundo", 2));
+console.log(cutString("Hola Mundo", "12"));
+console.log(cutString(3, 5));
 
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
-miFuncion3 = (string, separator) => console.log(string.split(separator));
-miFuncion3("hola que tal", " ");
+separarCadena = (string, separator) => (stringLength(string) > 0 ? string.split(separator) : "La cadena no se puede separar.");
+console.log(separarCadena("hola que tal", " "));
+console.log(separarCadena("holaquetal", " "));
+console.log(separarCadena("", ""));
+console.log(separarCadena("String", ","));
 
 // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
-miFuncion4 = (string, num) => {
-   let resultado = "";
-   for (let i = 0; i < num; i++) resultado += " " + string;
-   console.log(resultado);
-};
-miFuncion4("Hola Mundo", 3);
+repeatString = (string, i) => (stringLength(string) > 0 && Number.isInteger(i) ? `${string} `.repeat(i) : "Imposible repetir la cadena.");
+console.log(repeatString("Hola Mundo", "q"));
+console.log(repeatString(3, 3));
+console.log(repeatString("Hola Mundo", 3));
+console.log(repeatString("Cadena repetida", 4));
